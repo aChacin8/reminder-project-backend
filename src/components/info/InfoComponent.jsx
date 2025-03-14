a97dbbcf927850edf64650c013253e118b96f991
mrMcAko <alechacin08@gmail.com>
2025-03-13 20:02:25 -0600

    Creacion del apartado informativo del Home

---
+++ src/components/info/InfoComponent.jsx
@@ -0,0 +1,14 @@
+import './InfoComponent.scss'
+
+const InfoComponent = () =>{
+    return (
+        <>
+            <div className='about'>
+                <h1 className='about__h1'>Taskly</h1>
+                <p className='about__p'>¿Cansado de olvidar fechas importantes o perderte eventos clave? Taskly es tu asistente personal para gestionar eventos de manera sencilla y eficiente. Asigna tus actividades a un calendario interactivo, recibe recordatorios y mantén todo bajo control.</p>
+            </div>
+        </>
+    )
+}
+
+export default InfoComponent;
