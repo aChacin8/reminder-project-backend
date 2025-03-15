d9760b87f14447d91729292d1b85804919164864
mrMcAko <alechacin08@gmail.com>
2025-03-15 01:11:09 -0600

    Arreglo de colores del boton

--- src/components/info/InfoComponent.jsx
+++ src/components/info/InfoComponent.jsx
@@ -1,6 +1,5 @@
 import { Card, CardBody, CardText, Button, CardTitle} from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import './InfoComponent.scss'
 
 const InfoComponent = () =>{
     return (
@@ -25,7 +24,7 @@ const InfoComponent = () =>{
                         }}>
                             ¿Cansado de olvidar fechas importantes o perderte eventos clave? Taskly es tu asistente personal para gestionar eventos de manera sencilla y eficiente. Asigna tus actividades a un calendario interactivo, recibe recordatorios y mantén todo bajo control.
                     </CardText>
-                    <NavLink to='/SignUp'><Button variant='primary' type='button' className='me-2'>Crear una cuenta</Button></NavLink>
+                    <NavLink to='/SignUp'><Button variant='primary' type='button' className='me-2' style={{ backgroundColor: ' rgb(47, 126, 245)'}}>Crear una cuenta</Button></NavLink>
                 </CardBody>
             </Card>
         </>
