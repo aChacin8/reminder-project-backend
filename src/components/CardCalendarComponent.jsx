e5e6a1c79b6640b9a3fbd5edf4f085df18cb600c
aChacin8 <alechacin08@gmail.com>
2025-03-25 11:50:02 -0600

    Agregamos información al componente para el evento

--- src/components/CardCalendarComponent.jsx
+++ src/components/CardCalendarComponent.jsx
@@ -2,27 +2,56 @@ import { Card, Button, Form } from 'react-bootstrap';
 
 const CardCalendarComponent = () => {
     return (
-        <Card className='cardCalendar'>
+        <Card className='cardCalendar' style={{width: '20rem', margin: '2rem'}}>
         <Card.Body>
             <Card.Title className='cardCalendar__title'>Crea un Evento</Card.Title>
             <Form 
             className='cardCalendar__form'>
                 <Form.Group className='cardCalendar__form-group'>
-                    <Form.Label>Titulo del evento:</Form.Label>
+                    <Form.Label>Titulo:</Form.Label>
                     <Form.Control
                         type='text'
                         name='title-e'
                         className='cardCalendar__group-title'
-                        placeholder='Ingresa un titulo de evento'
+                        placeholder='Ingresa un titulo del evento'
                     />
                 </Form.Group>
                 <Form.Group className='cardCalendar__form-group'>
-                    <Form.Label>Titulo del evento:</Form.Label>
+                    <Form.Label>Descripcion:</Form.Label>
                     <Form.Control
                         type='text'
-                        name='title-e'
-                        className='cardCalendar__group-title'
-                        placeholder='Ingresa un titulo de evento'
+                        name='descr-e'
+                        className='cardCalendar__group-descr'
+                        placeholder='Ingresa un descripcion del evento'
+                    />
+                </Form.Group>
+                <Form.Group className='d-flex m-3'>
+                    <Form.Group className='cardCalendar__form-group'>
+                        <Form.Label>Fecha de Inicio:</Form.Label>
+                        <Form.Control
+                            type='text'
+                            name='firstDate-e'
+                            className='cardCalendar__group-firstDate'
+                            placeholder='Ingresa un descripcion del evento'
+                        />
+                    </Form.Group>
+                    <Form.Group className='cardCalendar__form-group'>
+                        <Form.Label>Fecha Finalización:</Form.Label>
+                        <Form.Control
+                            type='text'
+                            name='lastDate-e'
+                            className='cardCalendar__group-lastDate'
+                            placeholder='Ingresa un descripcion del evento'
+                        />
+                    </Form.Group>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Color</Form.Label>
+                    <Form.Control
+                        type='color'
+                        name='color-e'
+                        className='cardCalendar__group-color'
+                        placeholder='Ingresa un color para el evento'
                     />
                 </Form.Group>
             </Form>
