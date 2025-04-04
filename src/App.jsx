07d1bc70499b55849ec3a0fbcfcf70536fa4176e
mrMcAko <alechacin08@gmail.com>
2025-04-03 19:07:53 -0600

    Llamamos al Provedor del contexto

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 import './App.css'
 import RoutesIndex from '@/routes/RoutesIndex'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '@/context/AuthContext'
 
 function App() {
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
         <RoutesIndex/>
       </BrowserRouter>
+    </AuthProvider>
+      
     </>
       
   )
