c96dd26b029a0942e42074f3f2f840cc557b534e
mrMcAko <alechacin08@gmail.com>
2025-04-03 19:10:54 -0600

    Arreglamos el contexto, pudiendo exportarlo ahora

--- src/hooks/useAuthContext.jsx
+++ src/hooks/useAuthContext.jsx
@@ -1,7 +1,7 @@
 import { AuthContext } from "@/context/AuthContext";
 import { useContext } from "react";
 
-const useAuthContext = () => {
+export const useAuthContext = () => {
     const context = useContext(AuthContext);
     if (!context){
         throw new Error ('useAuthContext debe ser utilizado dentro de AuthProvider');
