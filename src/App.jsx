7cdbe2a34dc12ce3406315cd60e309263e119935
mrMcAko <alechacin08@gmail.com>
2025-04-09 01:00:38 -0600

    Agregamos el proveedor del contexto de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import RoutesIndex from '@/routes/RoutesIndex'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from '@/context/AuthContext'
+import { EventProvider } from './context/EventContext'
 
 function App() {
 
   return (
     <>
+    
     <AuthProvider>
-      <BrowserRouter>
-        <RoutesIndex/>
-      </BrowserRouter>
+      <EventProvider>
+        <BrowserRouter>
+          <RoutesIndex/>
+        </BrowserRouter>
+      </EventProvider>
     </AuthProvider>
       
     </>
