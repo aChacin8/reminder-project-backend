fefa8407c4e21861b040b32eb7ece87be6a5f2c5
mrMcAko <alechacin08@gmail.com>
2025-05-06 15:56:32 -0600

    Modificamos el modelo de eventos, creando finalmente el CRUD de los eventos

--- models/Events.js
+++ models/Events.js
@@ -1,20 +1,50 @@
 const knex = require ('../config')
 
-const createEvent = (bodyEvent) => {
-    return knex
-        .insert(bodyEvent)
-        .into('events')
-        .returning('*')
+const createEvent = async (bodyEvent) => {
+    const [id] = await knex('events')
+    .insert(bodyEvent); // Devuelve el id autoincremental
+    return knex('events')
+    .where('id_events', id)
+    .first(); // Retorna el evento recién creado
 }
 
-const viewAll = () => {
-    return knex
+const viewAll = async () => {
+    return knex('events')
         .select('*')
-        .from('events')
         .where('active', true)
+}
+
+const findById = async (idEvents) => {
+    return knex('events')
+        .select('*')
+        .where('id_events', idEvents)
+        .andWhere('active', true)
+}
+
+const updateEvent = async (idEvents, bodyEvents)=> {
+    return knex('events')
+        .where('id_events', idEvents)
+        .update(bodyEvents)
+        .andWhere('active', true)
+}
+
+const softDeleteEvent = async (idEvents)=> {
+    return knex('events')
+        .where('id_events', idEvents)
+        .update({active: false}) // Cambia el estado a inactivo
+}
+
+const destroyEvent = async (idEvents)=> {
+    return knex('events')
+        .where('id_events', idEvents)
+        .del() // Elimina el registro de la base de datos
 }
 
 module.exports = {
     createEvent,
-    viewAll
+    viewAll,
+    findById,
+    updateEvent,
+    softDeleteEvent,
+    destroyEvent
 }
