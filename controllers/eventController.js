ccd0ccd201dc23ca90896a55b2f38abe1c4aa4d1
mrMcAko <alechacin08@gmail.com>
2025-05-06 15:55:17 -0600

    Modificamos por completo la logica de creacion de usuario.
    Creamos los demas controladores, getAll, getById, Update, Delete

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,37 +1,126 @@
 const ModelEvent = require('../models/Events'); //Importa el modelo de eventos
 
-const createEvent = (req, res) => {
-    const {name, description, start_date, end_date, color} = req.body; //Desestructura el body de la peticion
-    const id_user = req.user.id_users; //id del usuario logueado
-    const event = {
-        id_users: id_user, 
-        event_name: name,
-        event_description: description,
-        event_start_date: start_date,
-        event_end_date: end_date,
-        color: color
-    }; //Crea un objeto con los datos del evento
-    
-    ModelEvent.createEvent(req.body) //Llama a la funcion createEvent del modelo
-            .then((event)=>{
-                res.status(201).json(event); //Devuelve el evento creado
-            })
-            .catch((error)=>{
-                res.status(500).json({message: 'Error al crear el evento', error}); //Devuelve un error si no se puede crear el evento
-            })
+const createEvent = async (req, res) => {
+    try {
+        console.log("Token:", token); // Muestra el token en la consola para depuración
+
+        console.log("REQ.BODY:", req.body);
+        console.log("REQ.USER:", req.user);
+
+        const { event_name, event_description, event_start_date, event_end_date, ...rest } = req.body; //Desestructura el body de la peticion
+        const event = await ModelEvent.createEvent({
+            ...rest,
+            name: event_name,
+            description: event_description,
+            start_date: event_start_date ,
+            end_date: event_end_date,
+            id_users: req.user.id_users //Agrega el id del usuario a la peticion
+        }); //Llama a la funcion createEvent del modelo
+        if (!event_name || !event_start_date || !event_end_date) { //Verifica que los campos no esten vacios
+            return res.status(400).json({ message: 'Faltan campos obligatorios' }); //Devuelve un error si faltan campos obligatorios
+        }
+        if (event_start_date > event_end_date) { //Verifica que la fecha de inicio sea menor a la fecha de fin
+            return res.status(400).json({ message: 'La fecha de inicio no puede ser mayor a la fecha de fin' }); //Devuelve un error si la fecha de inicio es mayor a la fecha de fin
+        }
+        if (event_start_date < new Date()) { //Verifica que la fecha de inicio no sea menor a la fecha actual
+            return res.status(400).json({ message: 'La fecha de inicio no puede ser menor a la fecha actual' }); //Devuelve un error si la fecha de inicio es menor a la fecha actual
+        }
+        res.status(201).json(event); //Devuelve el evento creado
+        console.log("Evento creado:", event); //Muestra el evento creado en la consola
+    } catch (error) {
+        res.status(400).json({ message: 'Error al crear el evento', error }); //Devuelve un error si no se puede crear el evento
+    }
 }
 
-const getAllEvents = (req, res) => {
-    ModelEvent.viewAll(req.user.id_users) //Llama a la funcion getAllEvents del modelo
-        .then((events)=>{
-            res.status(200).json(events); //Devuelve todos los eventos
-        })
-        .catch((error)=>{
-            res.status(500).json({message: 'Error al obtener los eventos', error}); //Devuelve un error si no se pueden obtener los eventos
-        }) 
+const getAllEvents = async (req, res) => {
+    try {
+        const events = await ModelEvent.viewAll(); //Llama a la funcion viewAll del modelo
+        res.status(200).json(events); //Devuelve todos los eventos
+        console.log("Eventos obtenidos:", events); //Muestra los eventos obtenidos en la consola
+    } catch (error) {
+        res.status(400).json({ message: 'Error al obtener los eventos', error }); //Devuelve un error si no se pueden obtener los eventos
+    }
+}
+
+const getEventById = async (req,res)=> {
+    try {
+        const event = await ModelEvent.getEventById(req.params.id_events); //Llama a la funcion getEventById del modelo
+        if (!event){
+            return res.status(404).json({ message: 'Evento no encontrado' }); //Devuelve un error si no se encuentra el evento
+        }
+    } catch (error) {
+        res.status(400).json({ message: 'Error al obtener el evento, en getEvent', error }); //Devuelve un error si no se puede obtener el evento
+    }
+}
+
+const updateEvent = async (req, res) => {
+    try {
+        const {id_events} = req.params; //Desestructura el id del evento de la peticion
+        const { event_name, event_description, event_start_date, event_end_date, ...rest } = req.body; //Desestructura el body de la peticion
+        
+        const existingEvent = await ModelEvent.getEventById(id_events); //Llama a la funcion getEventById del modelo
+        if (!existingEvent) {
+            return res.status(404).json({ message: 'Evento no encontrado' }); //Devuelve un error si no se encuentra el evento
+        }
+
+        const event = await ModelEvent.createEvent({
+            ...rest,
+            name: event_name,
+            description: event_description,
+            start_date: event_start_date ,
+            end_date: event_end_date,
+            id_users: req.user.id_users //Agrega el id del usuario a la peticion
+        });  //Llama a la funcion updateEvent del modelo
+        if(!event_name || !event_start_date || !event_end_date) { //Verifica que los campos no esten vacios
+            return res.status(400).json({ message: 'Faltan campos obligatorios' }); //Devuelve un error si faltan campos obligatorios
+        }
+        if (event_start_date > event_end_date) { //Verifica que la fecha de inicio sea menor a la fecha de fin
+            return res.status(400).json({ message: 'La fecha de inicio no puede ser mayor a la fecha de fin' }); //Devuelve un error si la fecha de inicio es mayor a la fecha de fin
+        }
+        if (event_start_date < new Date()) { //Verifica que la fecha de inicio no sea menor a la fecha actual
+            return res.status(400).json({ message: 'La fecha de inicio no puede ser menor a la fecha actual' }); //Devuelve un error si la fecha de inicio es menor a la fecha actual
+        }
+
+        res.status(200).json(event);
+        console.log("Evento actualizado:", event); //Muestra el evento actualizado en la consola
+    } catch (error) {
+        res.status(400).json({ message: 'Error al actualizar el evento', error }); //Devuelve un error si no se puede actualizar el evento
+    }
+}
+
+const softDeleteEvent = async (req,res)=> {
+    try {
+        const {id_events} = req.params; //Desestructura el id del evento de la peticion
+        const event = await ModelEvent.deleteEvent(id_events); //Llama a la funcion deleteEvent del modelo
+        
+        if (!event) {
+            return res.status(404).json({ message: 'Evento no encontrado' }); //Devuelve un error si no se encuentra el evento
+        }
+        res.status(200).json({ message: 'Evento eliminado' }); //Devuelve un mensaje
+    } catch (error) {
+        res.status(400).json({ message: 'Error al eliminar el evento', error }); //Devuelve un error si no se puede eliminar el evento
+    }
+}
+
+const destroyEvent = async (req,res)=> {
+    try {
+        const {id_events} = req.params; //Desestructura el id del evento de la peticion
+        const event = await ModelEvent.destroyEvent(id_events); //Llama a la funcion destroyEvent del modelo
+        
+        if (!event) {
+            return res.status(404).json({ message: 'Evento no encontrado' }); //Devuelve un error si no se encuentra el evento
+        }
+        res.status(200).json({ message: 'Evento eliminado' }); //Devuelve un mensaje
+    } catch (error) {
+        res.status(400).json({ message: 'Error al eliminar el evento', error }); //Devuelve un error si no se puede eliminar el evento
+    }
 }
 
 module.exports = {
     createEvent,
-    getAllEvents
+    getAllEvents,
+    getEventById,
+    updateEvent,
+    softDeleteEvent,
+    destroyEvent
 }
