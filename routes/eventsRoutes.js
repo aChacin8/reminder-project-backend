1202a73216ed9a5f67b940f9f4abc2c3c592583a
mrMcAko <alechacin08@gmail.com>
2025-05-06 15:57:32 -0600

    Agregamos nuevas rutas, para los controladores creados. Además modificamos la ruta post para que haga uso del middleware del token

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const eventRoutes = express.Router();
 const eventController = require('../controllers/eventController');
+const verifyToken = require('../middlewares/authMiddleware'); // Importa el middleware de verificación de token
 
-eventRoutes.post('/events', eventController.createEvent); // Crear un nuevo evento
+eventRoutes.post('/events', verifyToken, eventController.createEvent); // Crear un nuevo evento
 eventRoutes.get('/events', eventController.getAllEvents); // Obtener todos los eventos
+eventRoutes.get('events/:id_events', eventController.getEventById); // Obtener un evento por ID
+eventRoutes.patch('events/:id_events',eventController.updateEvent); // Actualizar un evento por ID
+eventRoutes.delete('events/:id_events', eventController.softDeleteEvent); // Eliminar un evento por ID (soft delete)
+eventRoutes.delete('events/:id_events/hard', eventController.destroyEvent); // Eliminar un evento por ID (hard delete)
 
 module.exports = eventRoutes;
