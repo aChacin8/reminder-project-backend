f5f44cc936d892f41816868b151719cf75eaf8ff
mrMcAko <alechacin08@gmail.com>
2025-05-08 22:31:14 -0600

    Agregamos todas las rutas para el token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const userRoutes = express.Router()
-const userController = require('../controllers/userController')
-const authController = require('../controllers/authController')
+    const express = require('express')
+    const userRoutes = express.Router()
+    const userController = require('../controllers/userController')
+    const authController = require('../controllers/authController')
+    const verifyToken = require('../middlewares/authMiddleware')
 
-userRoutes.post('/users', userController.createUser) // Crear un nuevo usuario
-userRoutes.get('/users', userController.viewAllUsers) // Visualizar todos los usuarios
+    userRoutes.post('/users', userController.createUser) // Crear un nuevo usuario
+    userRoutes.get('/users', userController.viewAllUsers) // Visualizar todos los usuarios
 
-userRoutes.post('/register', authController.registerUser) // Registrar un nuevo usuario
-userRoutes.post('/login',authController.loginUser) // Iniciar sesión
-module.exports = userRoutes
+    userRoutes.post('/register', authController.registerUser) // Registrar un nuevo usuario
+    userRoutes.post('/login',authController.loginUser) // Iniciar sesión
+
+    userRoutes.get('/token', authController.getTokenByEmail) // Obtener el token por email
+    userRoutes.get('/verify-token', verifyToken, (req, res) => {
+        res.status(200).json({ message: 'Token válido' }) // Verificar el token
+    }) // Verificar el token
+
+    module.exports = userRoutes
