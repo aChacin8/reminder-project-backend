33cb5fd46ccbb65b2b562c4e0b34a03ffe1acf2a
mrMcAko <alechacin08@gmail.com>
2025-05-09 14:39:31 -0600

    Agregamos la referencia de los eventos expirados

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const userRoutes = require('./routes/userRoutes')
 const eventRoutes = require('./routes/eventsRoutes')
+const deleteExpiredEvents = require('./cron/deleteExpiredEvents') // Importa el cron para eliminar eventos expirados
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +12,9 @@ app.use(cors()) // Permite el acceso a la API desde cualquier origen
 app.use(express.json()) //Maneja el body de las peticiones en formato JSON
 app.use('/taskly', userRoutes) //Rutas de la API usuarios
 app.use('/taskly', eventRoutes) //Rutas de la API eventos
+
+deleteExpiredEvents(); // Inicia el cron para eliminar eventos expirados
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`) //Se imprime el puerto en el que corre el servidor
