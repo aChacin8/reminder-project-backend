93d46970143c72051b615889fd00f45e026b451d
mrMcAko <alechacin08@gmail.com>
2025-05-08 22:31:48 -0600

    Mejoramos las rutas agregando el middleware del token a estas

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -4,7 +4,7 @@ const eventController = require('../controllers/eventController');
 const verifyToken = require('../middlewares/authMiddleware'); // Importa el middleware de verificación de token
 
 eventRoutes.post('/events', verifyToken, eventController.createEvent); // Crear un nuevo evento
-eventRoutes.get('/events', eventController.getAllEvents); // Obtener todos los eventos
+eventRoutes.get('/events',verifyToken, eventController.getAllEvents); // Obtener todos los eventos
 eventRoutes.get('events/:id_events', eventController.getEventById); // Obtener un evento por ID
 eventRoutes.patch('events/:id_events',eventController.updateEvent); // Actualizar un evento por ID
 eventRoutes.delete('events/:id_events', eventController.softDeleteEvent); // Eliminar un evento por ID (soft delete)
