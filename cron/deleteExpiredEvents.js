30b8d2eedc1600c619a252ad049a305fa8a1424f
mrMcAko <alechacin08@gmail.com>
2025-05-09 14:39:18 -0600

    Agregamos la funcion que usa cron, para eliminar eventos expirados

---
+++ cron/deleteExpiredEvents.js
@@ -0,0 +1,20 @@
+const cron = require('node-cron');
+const knex = require ('../config')
+
+const deleteExpiredEvents = async () => {
+    cron.schedule('0 * * * *', async () => { // Se ejecuta cada minuto
+    const now = new Date(); // Obtiene la fecha y hora actual
+
+    try {
+        const deleted = await knex('events')
+            .where('event_end_date', '<', now) // Filtra los eventos que han terminado
+            .del(); // Elimina los eventos que han terminado
+    console.log(`[CRON] Eventos expirados eliminados: ${deleted}, en ${now}`); // Muestra en consola los eventos eliminados
+    
+    } catch (error) {
+        console.log(`[CRON] Error al eliminar eventos expirados: ${error.message}`);
+    }
+}) 
+}
+
+module.exports = deleteExpiredEvents; // Exporta la funcion deleteExpiredEvents
