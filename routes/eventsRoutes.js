63399c32d68005785d680f83c5bb534c583224e4
mrMcAko <alechacin08@gmail.com>
2025-05-09 14:05:29 -0600

    Correccion de referencias erroneas en rutas

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -5,9 +5,9 @@ const verifyToken = require('../middlewares/authMiddleware'); // Importa el midd
 
 eventRoutes.post('/events', verifyToken, eventController.createEvent); // Crear un nuevo evento
 eventRoutes.get('/events',verifyToken, eventController.getAllEvents); // Obtener todos los eventos
-eventRoutes.get('events/:id_events', eventController.getEventById); // Obtener un evento por ID
-eventRoutes.patch('events/:id_events',eventController.updateEvent); // Actualizar un evento por ID
-eventRoutes.delete('events/:id_events', eventController.softDeleteEvent); // Eliminar un evento por ID (soft delete)
-eventRoutes.delete('events/:id_events/hard', eventController.destroyEvent); // Eliminar un evento por ID (hard delete)
+eventRoutes.get('/events/:id_events', eventController.getEventById); // Obtener un evento por ID
+eventRoutes.patch('/events/:id_events',eventController.updateEvent); // Actualizar un evento por ID
+eventRoutes.delete('/events/:id_events', eventController.softDeleteEvent); // Eliminar un evento por ID (soft delete)
+eventRoutes.delete('/events/:id_events/hard', eventController.destroyEvent); // Eliminar un evento por ID (hard delete)
 
 module.exports = eventRoutes;
