f4fab6123565c9aa7e282dd723c978bead46dc2f
mrMcAko <alechacin08@gmail.com>
2025-05-13 16:45:33 -0600

    Agregamos logica de websockets al index para correr el server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 process.loadEnvFile() // Carga las varibles de entorno del archivo .env
-const express = require('express')
-const cors = require('cors')
+
 const userRoutes = require('./routes/userRoutes')
 const eventRoutes = require('./routes/eventsRoutes')
 const deleteExpiredEvents = require('./cron/deleteExpiredEvents') // Importa el cron para eliminar eventos expirados
+const initWebSocket = require('./services/websockets')
+
+const express = require('express')
+const cors = require('cors')
+const http = require ('http')
 
 const app = express()
+const server = http.createServer(app) // 
 const port = process.env.PORT || 3000
 
 app.use(cors()) // Permite el acceso a la API desde cualquier origen
@@ -14,9 +19,9 @@ app.use('/taskly', userRoutes) //Rutas de la API usuarios
 app.use('/taskly', eventRoutes) //Rutas de la API eventos
 
 deleteExpiredEvents(); // Inicia el cron para eliminar eventos expirados
+initWebSocket(server);
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`) //Se imprime el puerto en el que corre el servidor
   console.log(`http://localhost:${port}`) 
 })
