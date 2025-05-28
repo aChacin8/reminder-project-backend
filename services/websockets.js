e897d1b8b027faa6710f522760945af6bce244dd
mrMcAko <alechacin08@gmail.com>
2025-05-27 23:54:42 -0600

    fix: Arreglamos el websocket, enviando las notificaciones únicas de cada evento para cada usuario

--- services/websockets.js
+++ services/websockets.js
@@ -1,15 +1,28 @@
-const socketIo = require('socket.io');
 const cron = require('node-cron');
 const knex = require('../config');
+const socketIo = require('socket.io');
 
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'; // URL del frontend
 let io; // Para acceder a la instancia desde otras partes
 
 const initWebSocket = (server) => {
     io = socketIo(server, {
         cors: {
-            origin: '*',
-            methods: ['GET', 'POST']
+            origin: `${FRONTEND_URL}`,
+            methods: ['GET', 'POST'],
+            credentials: true
         }
+    });
+
+    io.on('connection', (socket) => {
+        console.log('Usuario conectado al WebSocket');
+
+        socket.on('join-user-room', (userId) => {
+            if (userId) {
+                console.log(`Usuario ${userId} se unió a la sala: ${userId}`);
+                socket.join(userId.toString());
+            }
+        });
     });
 
     // Cron para verificar eventos que expiran en las siguientes 24 horas
@@ -18,21 +31,23 @@ const initWebSocket = (server) => {
 
         const now = new Date();
         const in24hrs = new Date(now.getTime() + 24 * 60 * 60 * 1000);
-
-        const events = await knex('events')
-            .whereBetween('event_end_date', [now.toISOString(), in24hrs.toISOString()]);
-
-        if (events.length === 0) {
-            console.log('No hay eventos por vencer');
-        } else {
-            events.forEach((event) => {
-                console.log(`Enviando notificación para evento: ${event.event_name}`);
-                io.emit('event-expiring', {
-                    message: `El evento "${event.event_name}" está por vencer.`,
-                    eventId: event.id_event,
-                    userId: event.id_users
+        try {
+            const events = await knex('events')
+                .whereBetween('event_end_date', [now.toISOString(), in24hrs.toISOString()])
+            if (events.length === 0) {
+                console.log('No hay eventos por vencer');
+            } else {
+                events.forEach((event) => {
+                    console.log(`Enviando notificación a sala ${event.id_users}`);
+                    io.to(event.id_users.toString()).emit('event-expiring', {
+                        message: `El evento "${event.event_name}" está por vencer.`,
+                        eventId: event.id_events,
+                        userId: event.id_users
+                    });
                 });
-            });
+            }
+        } catch (error) {
+            console.error('Error al verificar eventos por vencerse:', error);
         }
     });
 };
