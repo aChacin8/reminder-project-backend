85ca1ac619d307651891d4f1b783d6c17ae72f47
mrMcAko <alechacin08@gmail.com>
2025-06-02 00:16:17 -0600

    fix: Agregamos referencia necesarias de "encrypt"

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const ModelUsers = require('../models/Users')
-const { decrypt } = require('../utils/crypto');
+const { decrypt, encrypt } = require('../utils/crypto');
 
 const createUser = async (req, res) => {
     try {
@@ -30,7 +30,7 @@ const findById = async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: 'Usuario no encontrado' });
         }
-        
+
         const decryptedUser = {
             ...user, 
             address: decrypt(user.address),
@@ -43,8 +43,53 @@ const findById = async (req, res) => {
     }
 };
 
+const updateUser = async (req,res) => {
+    const { idUsers } = req.params;
+    const { first_name, last_name, address, phone_num } = req.body;
+
+    try {
+        const user = await ModelUsers.findById(idUsers);
+        if (!user) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        console.log('Usuario antes de actualizar:', user);
+
+        const encryptedAddress = encrypt(address);
+        const encryptedPhone = encrypt(phone_num);
+
+        console.log('Datos cifrados a actualizar:', { 
+            first_name, 
+            last_name, 
+            address: encryptedAddress, 
+            phone_num: encryptedPhone 
+        });
+
+        const updatedUser = await ModelUsers.updateUser(idUsers, {
+            first_name,
+            last_name,
+            address: encryptedAddress,
+            phone_num: encryptedPhone
+        });
+
+        console.log('Usuario actualizado en BD:', updatedUser);
+
+        const {created_at, ...dataToUpdate} = {
+            ...updatedUser,
+            address: decrypt(updatedUser.address),
+            phone_num: decrypt(updatedUser.phone_num),
+        };
+
+        return res.status(200).json(dataToUpdate);
+    } catch (error) {
+        console.error('Error en updateUser:', error);
+        return res.status(400).json({ message: 'Error al actualizar el usuario', error });
+    }
+};
+
 module.exports = {
     createUser,
     viewAllUsers,
-    findById
+    findById,
+    updateUser
 }
