f778e76c1c589ab120ef2ceccb1e422d8a4587e2
mrMcAko <alechacin08@gmail.com>
2025-06-02 12:04:26 -0600

    feat: Arreglo de webSocket para manejar las notificaciones cada 15 minutos.

--- services/websockets.js
+++ services/websockets.js
@@ -25,22 +25,24 @@ const initWebSocket = (server) => {
         });
     });
 
-    // Cron para verificar eventos que expiran en las siguientes 24 horas
-    cron.schedule('0 * * * *', async () => {
+    // Cron para verificar eventos que expiran en los siguientes 15 minutos
+    cron.schedule('/15 * * * *', async () => {
         console.log('Verificando eventos por vencerse...');
 
         const now = new Date();
         const in24hrs = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+        const in15mins = new Date(now.getTime() + 15 * 60 * 1000);
+
         try {
             const events = await knex('events')
-                .whereBetween('event_end_date', [now.toISOString(), in24hrs.toISOString()])
-            if (events.length === 0) {
+                .whereBetween('event_end_date', [now, in15mins])
+                if (events.length === 0) {
                 console.log('No hay eventos por vencer');
-            } else {
-                events.forEach((event) => {
+                } else {
+                    events.forEach((event) => {
                     console.log(`Enviando notificación a sala ${event.id_users}`);
                     io.to(event.id_users.toString()).emit('event-expiring', {
-                        message: `El evento "${event.event_name}" está por vencer.`,
+                        message: `El evento "${event.event_name}" está por vencer (En menos de 15 minutos). El dia ${new Date(event.event_end_date).toLocaleString()}`, 
                         eventId: event.id_events,
                         userId: event.id_users
                     });
