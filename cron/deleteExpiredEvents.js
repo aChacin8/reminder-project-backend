e2c96b0583ab0cd451fb52d8dbb83452d22da877
mrMcAko <alechacin08@gmail.com>
2025-09-17 16:14:26 -0600

    fix: Arreglo de importacion

--- cron/deleteExpiredEvents.js
+++ cron/deleteExpiredEvents.js
@@ -1,20 +1,19 @@
 const cron = require('node-cron');
-const knex = require ('../config')
-
+const knex = require('../config'); 
 const deleteExpiredEvents = async () => {
-    cron.schedule('* * * * *', async () => { // Se ejecuta cada minuto
-    const now = new Date(); // Obtiene la fecha y hora actual
+  cron.schedule('* * * * *', async () => { // se ejecuta cada minuto
+    const now = new Date();
 
     try {
-        const deleted = await knex('events')
-            .where('event_end_date', '<', now) // Filtra los eventos que han terminado
-            .del(); // Elimina los eventos que han terminado
-    console.log(`[CRON] Eventos expirados eliminados: ${deleted}, en ${now}`); // Muestra en consola los eventos eliminados
-    
-    } catch (error) {
-        console.log(`[CRON] Error al eliminar eventos expirados: ${error.message}`);
-    }
-}) 
-}
+      const deleted = await knex('events')
+        .where('event_end_date', '<', now)
+        .del();
 
-module.exports = deleteExpiredEvents; // Exporta la funcion deleteExpiredEvents
+      console.log(`[CRON] Eventos expirados eliminados: ${deleted}, en ${now}`);
+    } catch (error) {
+      console.log(`[CRON] Error al eliminar eventos expirados: ${error.message}`);
+    }
+  });
+};
+
+module.exports = deleteExpiredEvents;
